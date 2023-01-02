24c70b64f5893159f046a9ddcc98658efade2e70
Zheng <zhegyelau@hotmail.com>
2023-01-02 21:19:20 +0800

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test
-          Edit <code>src/App.js</code> and save to reload.
+          敬请期待
         </p>
         <a
           className="App-link"
